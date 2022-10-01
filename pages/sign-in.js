0bab6b2c4873f1716e6f573e85af0e372795ca93
Liu He <lhe19@yahoo.co.uk>
2022-10-01 22:31:41 +0100

    working on sign in page

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -26,7 +26,7 @@ export default function SignIn() {
                   type='email'
                   autoComplete='email'
                   required
-                  className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                  className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
                 />
               </div>
             </div>
@@ -45,7 +45,7 @@ export default function SignIn() {
                   type='password'
                   autoComplete='current-password'
                   required
-                  className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                  className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
                 />
               </div>
             </div>
@@ -56,7 +56,7 @@ export default function SignIn() {
                   id='remember-me'
                   name='remember-me'
                   type='checkbox'
-                  className='h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
+                  className='h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded'
                 />
                 <label
                   htmlFor='remember-me'
@@ -69,7 +69,7 @@ export default function SignIn() {
               <div className='text-sm'>
                 <a
                   href='#'
-                  className='font-medium text-indigo-600 hover:text-indigo-500'
+                  className='font-medium text-blue-600 hover:text-blue-500'
                 >
                   Forgot your password?
                 </a>
@@ -79,7 +79,7 @@ export default function SignIn() {
             <div>
               <button
                 type='submit'
-                className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
               >
                 Sign in
               </button>
