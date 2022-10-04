62c1f8a679bdba5e686ca29de65e722f4f13134e
Liu He <lhe19@yahoo.co.uk>
2022-10-04 21:05:46 +0100

    added register page

--- pages/register.js
+++ pages/register.js
@@ -1,3 +1,141 @@
+import Link from 'next/link';
+import { useState } from 'react';
+
 export default function Register() {
-  return <div>register</div>;
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {};
+
+  return (
+    <section className='min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+        <h2 className='mt-6 text-center text-3xl font-extrabold text-stone-800'>
+          Create New Account
+        </h2>
+      </div>
+
+      <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+        <div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+          <form className='space-y-6' onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor='name'
+                className='block text-sm font-medium text-stone-700'
+              >
+                Full Name
+              </label>
+              <div className='mt-1'>
+                <input
+                  id='name'
+                  name='name'
+                  type='text'
+                  autoComplete='email'
+                  required
+                  className='appearance-none block w-full px-3 py-2 border border-stone-300 rounded-md shadow-sm placeholder-stone-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor='email'
+                className='block text-sm font-medium text-stone-700'
+              >
+                Email address
+              </label>
+              <div className='mt-1'>
+                <input
+                  id='email'
+                  name='email'
+                  type='email'
+                  autoComplete='email'
+                  required
+                  className='appearance-none block w-full px-3 py-2 border border-stone-300 rounded-md shadow-sm placeholder-stone-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor='password'
+                className='block text-sm font-medium text-stone-700'
+              >
+                Password
+              </label>
+              <div className='mt-1'>
+                <input
+                  id='password'
+                  name='password'
+                  type='password'
+                  autoComplete='current-password'
+                  required
+                  className='appearance-none block w-full px-3 py-2 border border-stone-300 rounded-md shadow-sm placeholder-stone-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor='confirmPassword'
+                className='block text-sm font-medium text-stone-700'
+              >
+                Confirm password
+              </label>
+              <div className='mt-1'>
+                <input
+                  id='confirmPassword'
+                  name='confirmPassword'
+                  type='password'
+                  autoComplete='current-password'
+                  required
+                  className='appearance-none block w-full px-3 py-2 border border-stone-300 rounded-md shadow-sm placeholder-stone-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type='submit'
+                className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
+              >
+                Create account
+              </button>
+            </div>
+          </form>
+
+          <div className='mt-6'>
+            <div className='relative'>
+              <div className='absolute inset-0 flex items-center'>
+                <div className='w-full border-t border-stone-300' />
+              </div>
+              <div className='relative flex justify-center text-sm'>
+                <span className='px-2 bg-white text-stone-500'>
+                  Already have an account?
+                </span>
+              </div>
+            </div>
+
+            <div className='mt-6'>
+              <Link href='#'>
+                <a className='w-full inline-flex justify-center py-2 px-4 border border-stone-300 rounded-md shadow-sm bg-white text-sm font-medium text-stone-500 hover:bg-stone-100'>
+                  Sign in
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- pages/sign-in.js
+++ pages/sign-in.js
@@ -11,6 +11,10 @@ export default function SignIn() {
 
   const router = useRouter();
 
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { email, password } = values;
@@ -20,13 +24,9 @@ export default function SignIn() {
       redirect: false,
     });
 
-    if (response.status === 200) router.back();
+    if (response.status === 200) return router.back();
 
-    console.log('incorrect user');
-  };
-
-  const handleChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
+    alert('Incorrect email or password');
   };
 
   return (
@@ -43,7 +43,7 @@ export default function SignIn() {
             <div>
               <label
                 htmlFor='email'
-                className='block text-sm font-medium text-gray-700'
+                className='block text-sm font-medium text-stone-700'
               >
                 Email address
               </label>
@@ -54,7 +54,7 @@ export default function SignIn() {
                   type='email'
                   autoComplete='email'
                   required
-                  className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                  className='appearance-none block w-full px-3 py-2 border border-stone-300 rounded-md shadow-sm placeholder-stone-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
                   onChange={handleChange}
                 />
               </div>
@@ -63,7 +63,7 @@ export default function SignIn() {
             <div>
               <label
                 htmlFor='password'
-                className='block text-sm font-medium text-gray-700'
+                className='block text-sm font-medium text-stone-700'
               >
                 Password
               </label>
@@ -74,7 +74,7 @@ export default function SignIn() {
                   type='password'
                   autoComplete='current-password'
                   required
-                  className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                  className='appearance-none block w-full px-3 py-2 border border-stone-300 rounded-md shadow-sm placeholder-stone-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
                   onChange={handleChange}
                 />
               </div>
@@ -86,11 +86,11 @@ export default function SignIn() {
                   id='remember-me'
                   name='remember-me'
                   type='checkbox'
-                  className='h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded'
+                  className='h-4 w-4 text-blue-600 focus:ring-blue-500 border-stone-300 rounded'
                 />
                 <label
                   htmlFor='remember-me'
-                  className='ml-2 block text-sm text-gray-900'
+                  className='ml-2 block text-sm text-stone-900'
                 >
                   Remember me
                 </label>
@@ -119,10 +119,10 @@ export default function SignIn() {
           <div className='mt-6'>
             <div className='relative'>
               <div className='absolute inset-0 flex items-center'>
-                <div className='w-full border-t border-gray-300' />
+                <div className='w-full border-t border-stone-300' />
               </div>
               <div className='relative flex justify-center text-sm'>
-                <span className='px-2 bg-white text-gray-500'>
+                <span className='px-2 bg-white text-stone-500'>
                   Or continue with
                 </span>
               </div>
@@ -132,7 +132,7 @@ export default function SignIn() {
               <div>
                 <a
                   href='#'
-                  className='w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50'
+                  className='w-full inline-flex justify-center py-2 px-4 border border-stone-300 rounded-md shadow-sm bg-white text-sm font-medium text-stone-500 hover:bg-stone-50'
                 >
                   <span className='sr-only'>Sign in with Facebook</span>
                   <svg
@@ -153,7 +153,7 @@ export default function SignIn() {
               <div>
                 <a
                   href='#'
-                  className='w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50'
+                  className='w-full inline-flex justify-center py-2 px-4 border border-stone-300 rounded-md shadow-sm bg-white text-sm font-medium text-stone-500 hover:bg-stone-50'
                 >
                   <span className='sr-only'>Sign in with Twitter</span>
                   <svg
@@ -170,7 +170,7 @@ export default function SignIn() {
               <div>
                 <a
                   href='#'
-                  className='w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50'
+                  className='w-full inline-flex justify-center py-2 px-4 border border-stone-300 rounded-md shadow-sm bg-white text-sm font-medium text-stone-500 hover:bg-stone-50'
                 >
                   <span className='sr-only'>Sign in with GitHub</span>
                   <svg
