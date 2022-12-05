3745314277fe3da88f152f79516482e6cce1194e
Liu He <lhe19@yahoo.co.uk>
2022-12-05 00:26:52 +0000

    added home page grid

--- components/Home/Grid.js
+++ components/Home/Grid.js
@@ -3,70 +3,64 @@ import Link from 'next/link';
 
 const data = [
   {
+    title: 'Buds Gen X',
+    subtitle: 'Big sound in small body',
+    url: '/images/grid_buds.png',
+    bgColor: 'bg-stone-50',
+    textColor: '',
+  },
+  {
     title: 'Milkyway Tab X',
     subtitle: 'Imagination beyond galaxies',
     url: '/images/grid_tablet.png',
-    bg: '',
-  },
-  {
-    title: 'Buds Gen X',
-    subtitle: 'Big sound in small bodies',
-    url: '/images/grid_buds.png',
-    bg: '',
+    bgColor: 'bg-sky-50',
+    textColor: '',
   },
   {
     title: 'Soho Blast',
     subtitle: 'This is where the party is at',
     url: '/images/grid_speakers.png',
-    bg: '',
+    bgColor: 'bg-orange-50',
+    textColor: '',
   },
   {
-    title: 'Rapid Wireless Charger',
+    title: 'Rapid Charger',
     subtitle: 'Unleash the ultimate power',
     url: '/images/grid_charger.png',
-    bg: '',
+    bgColor: 'bg-stone-50',
+    textColor: '',
   },
 ];
 
 function Grid() {
   return (
-    <div className='w-full grid overflow-hidden grid-cols-1 grid-rows-4 gap-2'>
+    <div className='w-full grid overflow-hidden grid-cols-1 grid-rows-4 md:gap-3 md:grid-cols-2 md:grid-rows-2 md:py-3'>
       {data.map((i) => (
         <div
           key={i.title}
-          className='bg-stone-300 flex flex-col justify-between items-center h-[530px]'
+          className={`${i.bgColor} 'flex flex-col justify-between items-center'`}
         >
-          <div className='flex flex-col justify-center items-center text-center my-16 md:my-24'>
+          <div className='flex flex-col justify-center items-center text-center mt-16 mb-12 md:mb-20'>
             <h2 className='text-3xl font-semibold md:text-4xl lg:text-5xl'>
               {i.title}
             </h2>
             <h3 className='text-xl font-medium mt-4 md:text-2xl lg:text-3xl lg:mt-6'>
               {i.subtitle}
             </h3>
-            <div className='mt-8 text-base space-x-4 lg:space-x-6 lg:mt-16 lg:font-medium'>
+            <div className='mt-8 text-lg font-semibold text-indigo-700 space-x-6 lg:text-xl'>
               <Link href='/laptops'>
-                <a>
-                  <button
-                    type='button'
-                    className='text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:from-purple-600 focus:to-pink-600 rounded-lg px-4 py-1.5 text-center lg:px-5 lg:py-2'
-                  >
-                    Learn more
-                  </button>
+                <a className='hover:text-indigo-400 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-500 hover:to-pink-500'>
+                  Learn more
                 </a>
               </Link>
               <Link href='/laptops'>
-                <a>
-                  <button
-                    type='button'
-                    className='text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:from-pink-600 focus:to-orange-500 rounded-lg px-4 py-1.5 text-center lg:px-5 lg:py-2'
-                  >
-                    Shop now
-                  </button>
+                <a className='hover:text-indigo-400 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-500 hover:to-pink-500'>
+                  Shop now
                 </a>
               </Link>
             </div>
           </div>
-          <div className='relative mx-auto w-[250px] h-[250px]'>
+          <div className='relative mx-auto w-[260px] h-[260px] xl:w-[330px] xl:h-[330px]'>
             <Image src={i.url} layout='fill' objectFit='contain' />
           </div>
         </div>
