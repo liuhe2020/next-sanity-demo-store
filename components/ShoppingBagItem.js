9b8ad9432f1f43aedcfedf2c873b85bd13f9dfb9
Liu He <lhe19@yahoo.co.uk>
2022-12-18 23:38:51 +0000

    corrected text colour

--- components/ShoppingBagItem.js
+++ components/ShoppingBagItem.js
@@ -55,7 +55,7 @@ export default function ShoppingBagItem({ item }) {
       <div className='flex place-self-end'>
         <svg
           onClick={handleDecrement}
-          className='fill-current text-stone-600 w-3 cursor-pointer hover:text-black'
+          className='fill-current text-stone-600 w-3 cursor-pointer hover:text-stone-900'
           viewBox='0 0 448 512'
         >
           <path d='M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z' />
@@ -69,7 +69,7 @@ export default function ShoppingBagItem({ item }) {
         />
         <svg
           onClick={handleIncrement}
-          className='fill-current text-stone-600 w-3 cursor-pointer hover:text-black'
+          className='fill-current text-stone-600 w-3 cursor-pointer hover:text-stone-900'
           viewBox='0 0 448 512'
         >
           <path d='M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z' />
