e6fc8a8d978d64594892a6751f00f139d2e6754f
Liu He <lhe19@yahoo.co.uk>
2023-01-14 23:04:21 +0000

    merged sanity to root folder

---
+++ schemas/index.js
@@ -0,0 +1,19 @@
+import product from './product';
+import category from './category';
+import user from './user';
+import order from './order';
+import orderItem from './orderItem';
+import deliveryAddress from './deliveryAddress';
+import shipment from './shipment';
+import paymentDetail from './paymentDetail';
+
+export const schemaTypes = [
+  product,
+  category,
+  user,
+  order,
+  orderItem,
+  paymentDetail,
+  deliveryAddress,
+  shipment,
+];
