650f8432a2b4138a8136d509d7ea81f5411e5811
Liu He <lhe19@yahoo.co.uk>
2023-05-21 22:57:38 +0100

    testing nextauth middleware

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -12,7 +12,7 @@ export default function AccountPage() {
   const [orders, setOrders] = useState<Order[] | []>([]);
   const [view, setView] = useState('Orders');
 
-  const { data: session, status } = useSession({ required: true });
+  const { data: session, status } = useSession();
 
   useEffect(() => {
     const getOrders = async () => {
@@ -24,7 +24,7 @@ export default function AccountPage() {
     getOrders();
   }, [session]);
 
-  if (status === 'loading') return null;
+  // if (status === 'loading') return null;
 
   return (
     <section className='max-w-screen-lg px-4 pt-10 sm:pt-16 relative w-full mx-auto lg:px-8'>
---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+// export { default } from "next-auth/middleware";
+import { withAuth } from 'next-auth/middleware';
+import { NextRequest, NextResponse } from 'next/server';
+
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    console.log('token: ', req.nextauth.token);
+
+    // if (req.nextUrl.pathname.startsWith('/account') && !req.nextauth.token)
+    //   return NextResponse.rewrite(new URL('/auth/login?message=You Are Not Authorized!', req.url));
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
+
+export const config = {
+  matcher: ['/account/:path*'],
+};
