f1436b013bd633a217380010ae0e4964645637fd
Liu He <lhe19@yahoo.co.uk>
2023-09-24 18:56:52 +0100

    finished sorting ui comp

--- src/app/[category]/Sort.tsx
+++ src/app/[category]/Sort.tsx
@@ -1,47 +1,51 @@
 'use client';
 
-import { Fragment, useState } from 'react';
-import { Listbox, Transition } from '@headlessui/react';
+import { Fragment } from 'react';
+import { Popover, Transition } from '@headlessui/react';
+import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+import classNames from '@/utils/classNames';
 
-const list = [{ order: 'Name - A to Z' }, { order: 'Name - Z to A' }, { order: 'Price - low to high' }, { order: 'Price - high to low' }];
+const list = [
+  { order: 'Name - A to Z', param: 'name_asc' },
+  { order: 'Name - Z to A', param: 'name_desc' },
+  { order: 'Price - low to high', param: 'prcie_asc' },
+  { order: 'Price - high to low', param: 'prcie_desc' },
+];
 
 export default function Sort() {
-  const [selected, setSelected] = useState(list[0]);
+  const searchParams = useSearchParams();
+  const p = searchParams.get('sort');
+  const order = list.find((i) => i.param === p)?.order;
 
   return (
-    <div className=''>
-      <Listbox value={selected} onChange={setSelected}>
-        <div className='relative flex'>
-          <Listbox.Button className='relative w-full cursor-pointer rounded-xl ring-1 ring-stone-100 p-2.5 text-left xs:px-5 xs:w-[245px] xs:ml-auto lg:text-sm'>
-            <span className='block truncate'>Sort by: {selected.order}</span>
-            <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
-              {/* <ChevronUpDownIcon className='h-5 w-5 text-gray-400' aria-hidden='true' /> */}
-            </span>
-          </Listbox.Button>
-          <Transition as={Fragment} leave='transition ease-in duration-100' leaveFrom='opacity-100' leaveTo='opacity-0'>
-            <Listbox.Options className='absolute z-10 top-12 max-h-60 w-full bg-white overflow-auto py-1 text-base rounded-xl shadow ring-1 ring-stone-100 xs:w-[245px] right-0 lg:text-sm'>
+    <Popover className='relative flex'>
+      <Popover.Button className='relative w-full cursor-pointer rounded-xl outline-none ring-1 ring-stone-100 py-2.5 px-5 text-left xs:w-[255px] xs:ml-auto lg:text-sm lg:w-[230px]'>
+        <span className='block truncate'>Sort by: {order ?? 'Default'}</span>
+        <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2.5'>
+          <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1} stroke='currentColor' className='w-5 h-5'>
+            <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9' />
+          </svg>
+        </span>
+      </Popover.Button>
+      <Transition as={Fragment} leave='transition ease-in duration-100' leaveFrom='opacity-100' leaveTo='opacity-0'>
+        <Popover.Panel className='absolute z-10 top-12 max-h-60 w-full bg-white overflow-auto py-1 text-base rounded-xl shadow-lg ring-1 ring-stone-100 right-0 xs:w-[255px] lg:w-[230px] lg:text-sm'>
+          {({ close }) => (
+            <>
               {list.map((i) => (
-                <Listbox.Option
+                <Link
+                  href={`?sort=${i.param}`}
                   key={i.order}
-                  className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'}`}
-                  value={i}
+                  className={classNames(p === i.param && 'bg-sky-50', 'block relative select-none py-2 px-5 cursor-pointer')}
+                  onClick={() => close()}
                 >
-                  {({ selected }) => (
-                    <>
-                      <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>{i.order}</span>
-                      {selected ? (
-                        <span className='absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600'>
-                          {/* <CheckIcon className='h-5 w-5' aria-hidden='true' /> */}
-                        </span>
-                      ) : null}
-                    </>
-                  )}
-                </Listbox.Option>
+                  {i.order}
+                </Link>
               ))}
-            </Listbox.Options>
-          </Transition>
-        </div>
-      </Listbox>
-    </div>
+            </>
+          )}
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   );
 }
