f5fbd949e17dbafd35cfbb0e2f52778b2b157d73
Liu He <lhe19@yahoo.co.uk>
2023-10-27 23:22:00 +0100

    PayPalScriptProvider options fix

--- src/components/PayPalProvider.tsx
+++ src/components/PayPalProvider.tsx
@@ -1,11 +1,12 @@
 'use client';
+
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 export default function PayPalProvider({ children }: { children: React.ReactNode }) {
   return (
     <PayPalScriptProvider
       options={{
-        'client-id': process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID!,
+        clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID as string,
         components: 'buttons',
         currency: 'GBP',
       }}
