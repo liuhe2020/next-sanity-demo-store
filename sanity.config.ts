19a41adcd72c6dbeca67eb6b353180a2c8ab7ded
Liu He <lhe19@yahoo.co.uk>
2023-10-27 23:17:59 +0100

    next 14 & next-sanity update

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,22 +1,25 @@
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\studio\[[...index]]\page.tsx` route
+ */
+
+import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
-import { visionTool } from '@sanity/vision';
-import { schemaTypes } from './src/schemas';
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import { apiVersion, dataset, projectId } from './sanity/env';
+import { schemaTypes } from '@/schema';
 
 export default defineConfig({
   basePath: '/studio',
-  name: 'nsds-studio',
-  title: 'NSDS Studio',
-
   projectId,
   dataset,
-
-  plugins: [deskTool(), visionTool()],
-
-  schema: {
-    types: schemaTypes,
-  },
+  // Add and edit the content schema in the './sanity/schema' folder
+  schema: { types: schemaTypes },
+  plugins: [
+    deskTool(),
+    // Vision is a tool that lets you query your content with GROQ in the studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({ defaultApiVersion: apiVersion }),
+  ],
 });
