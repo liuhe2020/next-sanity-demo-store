6d108bd52ca5cf729d71ba7b0b1e3d361634023b
Liu He <lhe19@yahoo.co.uk>
2023-11-28 23:08:56 +0000

    changed skeleton animation style to shimmer

--- src/app/[category]/skeleton.tsx
+++ src/app/[category]/skeleton.tsx
@@ -2,11 +2,14 @@ export default function Skeleton() {
   return (
     <div className='grid gap-4 mx-auto xs:grid-cols-2 xs:gap-2.5 lg:grid-cols-3'>
       {Array.from({ length: 6 }, (_, index) => index + 1).map((i) => (
-        <div key={i} className='space-y-6 pb-[10%] w-full h-full bg-stone-50 rounded-2xl overflow-hidden animate-pulse'>
+        <div
+          key={i}
+          className='space-y-6 pb-[10%] w-full h-full bg-stone-50 rounded-2xl overflow-hidden relative before:absolute before:inset-0  before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:-translate-x-full before:via-white/40 before:to-transparent isolate before:border-t before:border-white/40 lg:pb-10 lg:h-[400px]'
+        >
           <div className='px-[10%] bg-[#eeedec]'>
             <div className='aspect-square' />
           </div>
-          <div className='space-y-2 px-[10%]'>
+          <div className='space-y-1.5 px-[10%]'>
             <div className='h-4 w-5/6 rounded-md bg-[#eeedec] mx-auto' />
             <div className='h-4 w-5/6 rounded-md bg-[#eeedec] mx-auto' />
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 
-export const content = ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'];
-export const theme = {
-  // define screens to enable max-width and dynamic breakpoints https://tailwindcss.com/blog/tailwindcss-v3-2
-  screens: {
-    xs: '550px',
-    sm: '640px',
-    md: '768px',
-    lg: '1024px',
-    xl: '1280px',
-    '2xl': '1536px',
+export default {
+  theme: {
+    screens: {
+      xs: '550px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    // options: https://github.com/theMosaad/tailwindcss-delicious-hamburgers/blob/main/src/index.js
+    deliciousHamburgers: {
+      size: '24px',
+      color: '#d6d3d1',
+      colorLight: '#fff8f4',
+      padding: '0px',
+      barSpacing: '7px',
+      animationSpeed: 1,
+    },
+    variants: {
+      fill: ['hover', 'focus'],
+    },
+    extend: {
+      keyframes: {
+        shimmer: {
+          '100%': {
+            transform: 'translateX(100%)',
+          },
+        },
+      },
+    },
   },
-  // options: https://github.com/theMosaad/tailwindcss-delicious-hamburgers/blob/main/src/index.js
-  deliciousHamburgers: {
-    size: '24px',
-    color: '#d6d3d1',
-    colorLight: '#fff8f4',
-    padding: '0px',
-    barSpacing: '7px',
-    animationSpeed: 1,
-  },
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  plugins: [require('@tailwindcss/forms'), require('tailwindcss-delicious-hamburgers'), require('tailwind-scrollbar')({ nocompatible: true })],
 };
-export const variants = {
-  fill: ['hover', 'focus'],
-};
-export const plugins = [require('@tailwindcss/forms'), require('tailwindcss-delicious-hamburgers'), require('tailwind-scrollbar')({ nocompatible: true })];
